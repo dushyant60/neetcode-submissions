d341ee211d0aa379846e49b37cb42ed99c539d50
Dushyant Singh <dushyantsom60@gmail.com>
2026-08-15 18:57:17 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,34 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        #  Brute Force 
+
+        # flag = False
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if nums[i]==nums[j]:
+        #             flag = True
+        # return flag
+
+        # freq_map = {}
+
+        # flag = False
+
+        # for i in range(n):
+        #     freq_map[nums[i]] = freq_map.get(nums[i],0)+1
+        
+        # for key, value in freq_map.items():
+        #     if value > 1:
+        #         flag = True
+        # return flag
+        
+        my_set = set()
+
+        for i in range(n):
+            if nums[i] in my_set:
+                return True
+            my_set.add(nums[i])
+        return False
+        
+
